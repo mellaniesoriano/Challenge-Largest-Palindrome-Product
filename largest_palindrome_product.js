de3bb426c64e5cb327c5bf11c521d48ddab21bcf
mellaniesoriano <msoriano808@gmail.com>
2017-06-03 10:35:15 -1000

    passes

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -6,12 +6,46 @@
  *                   the palindromeNumber and the palindromeNumber itself.
  */
 module.exports = function(digits){
+  // console.log(digits);
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
 
+  var numPlaces = [9, 99, 999, 9999, 99999];
+  var max = numPlaces[digits - 1];
+
   // do your work here
 
+  function isPalindrome(num) {
+    num = num.toString().split('');
+    for ( var i = 0; i < num.length / 2; i++ ) {
+      if ( num[i] !== num[num.length - 1 - i] ) {
+        return false;
+      }
+    }
+    return true;
+  }
+  console.log(isPalindrome(9009));
+
+  function largestPalNum() {
+    for ( var i = 0; i <= max; i++ ) {
+      for ( var k = 0; k <= max; k++) {
+        factor_0 = i;
+        factor_1 = k;
+        var product = i * k;
+        // console.log(product);
+
+        if ( isPalindrome(product) ) {
+          if ( product > palindromeNumber ) {
+          palindromeNumber = product;
+          console.log(isPalindrome(product));
+        }
+      }
+    }
+      console.log(factor_0 + ' x ' + factor_1 + ' = ' + product);
+    }
+  }
+  largestPalNum();
 
 
   return {
